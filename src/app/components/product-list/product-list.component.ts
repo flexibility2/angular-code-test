4676e6ecb251856dbca4a5e1a36d3857f9161265
flexibility2 <72940607@qq.com>
2025-02-05 20:59:07 +0800

    scroll to the selected element when refresh

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  Component,
+  OnInit,
+  ViewChildren,
+  QueryList,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import { Observable, combineLatest } from 'rxjs';
+import { filter, take } from 'rxjs/operators';
 
 import { ProductService, Product } from '../../services/product.service';
 
@@ -11,15 +19,34 @@ import { ProductService, Product } from '../../services/product.service';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterViewInit {
   products$!: Observable<Product[]>;
   selectedProductId$!: Observable<number | null>;
+  @ViewChildren('productItem') productItems!: QueryList<ElementRef>;
 
   constructor(private readonly productService: ProductService) {}
 
   ngOnInit(): void {
     this.products$ = this.productService.getProducts();
     this.selectedProductId$ = this.productService.selectedProductId$;
+  }
+
+  ngAfterViewInit(): void {
+    // 等待商品数据加载完成和选中ID都准备好
+    combineLatest([this.products$, this.selectedProductId$, this.productItems.changes])
+      .pipe(
+        filter(([products, id]) => products.length > 0 && id !== null),
+        take(1), // 只执行一次
+      )
+      .subscribe(([_, id]) => {
+        const element = this.productItems.find(item => item.nativeElement.id === `product-${id}`);
+        if (element) {
+          element.nativeElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center',
+          });
+        }
+      });
   }
 
   selectProduct(id: number): void {
